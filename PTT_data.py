613c8daa0aa694b25b43b2712845576130e7eb37
summer <k18382003@gmail.com>
2021-11-21 19:37:44 +0800

    PTT Web Crawl Project

--- PTT_data.py
+++ PTT_data.py
@@ -1,31 +1,101 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askdirectory
+from tkinter.messagebox import showerror
+import os
 import requests
-from  bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
-# for pageno in range(1,10):
-#     url = "https://www.ptt.cc/bbs/movie/index%s.html"%(pageno)
-#     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}
-#     res = requests.get(url, headers=headers)
-#     soup = BeautifulSoup(res.text, "html.parser")
-#     data_tags = soup.find_all("div",{"class":"title"})
-#     for i, item in enumerate(data_tags):
-#         print("Title : " + data_tags[i].a.text)
-#         print("Link : https://www.ptt.cc" + data_tags[i].a["href"])
-"====================================以下用Select改寫========================================="
-# data_tags = soup.select("div[class='title']")
-# for i, item in enumerate(data_tags):
-#     print("Title : " + data_tags[i].a.text)
-#     print("Link : https://www.ptt.cc/bbs/movie/" + data_tags[i].a["href"])
-pageno = 9500
-while(pageno >=9490):
-    url = "https://www.ptt.cc/bbs/movie/index%s.html" % (pageno)
-    headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, "html.parser")
-    data_tags = soup.find_all("div", {"class": "title"})
-    for i,item in enumerate(data_tags):
-        try:
-            print("Title : " + data_tags[i].a.text)
-            print("Link : https://www.ptt.cc" + data_tags[i].a["href"])
-        except AttributeError as e:
-            print("This article might have been removed.")
-    pageno -= 1
+# root window
+root = tk.Tk()
+root.title('PTT電影版爬蟲')
+var = tk.StringVar()
+var2 = tk.StringVar()
+root.resizable(False, False)
+
+strFilePath = tk.StringVar()
+
+def getFilePath():
+    FilePath = tk.filedialog.askdirectory()
+    if(FilePath!=''):
+        strFilePath.set(FilePath)
+
+def fetch_content(pagestart, pageend, savepath):
+    resource_path = savepath
+    if not os.path.exists(resource_path):
+        os.makedirs(resource_path)
+
+    while(pageend >= pagestart):
+        url = "https://www.ptt.cc/bbs/movie/index%s.html" % (pageend)
+        headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+        data_tags = soup.select("div[class='title']")
+        for item in data_tags:
+            try:
+                article_headline = item.a.text
+                article_link = "https://www.ptt.cc" + item.a["href"]
+                headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36"}
+                res = requests.get(article_link, headers=headers)
+                soup = BeautifulSoup(res.text, "html.parser")
+                article_content = soup.select("div[id='main-content']")[0].text.split("--")[0]
+                # 將內容載下來
+                with open(r'%s/%s.txt' % (resource_path, article_headline.replace(':', ' ')), 'w', encoding="UTF-8") as w:
+                    w.write(article_content)
+            except AttributeError as e:
+                print(e)
+            except FileNotFoundError as e:
+                pass
+            except OSError as e:
+                pass
+        pageend -= 1
+    tk.messagebox.showinfo('下載結果', '已下載完畢')
+
+
+# frame
+frame = ttk.Frame(root)
+
+
+# field options
+options = {'padx': 5, 'pady': 5}
+
+# temperature label
+lblcrawlpage = ttk.Label(frame, text='想要爬取的頁數')
+lblrange = ttk.Label(frame, text='從~到:')
+lblfilepath = ttk.Label(frame, text='存檔路徑:')
+lblcrawlpage.grid(column=0, row=0, sticky='W')
+lblrange.grid(column=0, row=1, sticky='W')
+lblfilepath.grid(column=0, row=2, sticky='W')
+
+
+# temperature entry
+start = tk.StringVar()
+start_entry = ttk.Entry(frame, textvariable=start)
+start_entry.grid(column=1, row=1)
+end = tk.StringVar()
+tilda = ttk.Label(frame, text='~')
+tilda.grid(column=2, row=1, sticky='W')
+end_entry = ttk.Entry(frame, textvariable=end)
+end_entry.grid(column=3, row=1)
+start_entry.focus()
+
+filepath_button = ttk.Button(frame, text='存檔路徑', command=getFilePath)
+filepath_button.grid(column=0, row=2, sticky='W')
+filepath_input = ttk.Entry(frame, textvariable=strFilePath, width=45)
+filepath_input.grid(columnspan=3, column=1, row=2, sticky='W')
+
+def Trigger_Fetch():
+    startpage = int(start.get())
+    endpage = int(end.get())
+    saveFilePage = strFilePath.get()
+    fetch_content(startpage, endpage, saveFilePage)
+
+confrim_button = ttk.Button(frame, text='開始爬文', command=Trigger_Fetch)
+confrim_button.grid(column=3, row=4, sticky='W')
+
+# add padding to the frame and show it
+frame.grid(padx=10, pady=10)
+
+
+# start the app
+root.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PTT_Web_Crawler
+This is a simple application that can allow users to climb Movie theme on PTT,
+and to download the content. This is the first version which can let user input the 
+pages they want to download with, also they can choose the save path and a progress
+bar while downloading the files.
+2nd version will let users choose different themes, also the content can be filtered
+by date not just page numbers.
+
+這是一個用Python搭配tkinter做出的簡易爬蟲工具. 目前第一版是可以讓使用者將PTT電影版的內容, 
+爬下來存在本機上. 目前使用者可以輸入要爬取的頁數, 以及存取的路徑. 在第二版將會讓使用者爬取不同的主題,
+而且還可以用日期區間來爬取, 並且在下載時會有下載進度條.
+
+IDE: PyCharm
+
+Language: Python(GUI package tkinter)
